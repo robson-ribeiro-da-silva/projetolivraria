2bf70180c5c0aa6aadc87672f34fadc21e88eb45
robson-ribeiro-da-silva <robson_r.silva@hotmail.com.br>
2019-05-01 10:11:12 -0300

    Commit inicial

---
+++ projetolivraria/src/main/java/br/edu/ifrn/projetolivraria/ProjetolivrariaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.projetolivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetolivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetolivrariaApplication.class, args);
+	}
+
+}
