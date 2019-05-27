5ecb530405f1b11857d6f67d09609f758fc14ce6
robson-ribeiro-da-silva <robson_r.silva@hotmail.com.br>
2019-05-27 10:45:01 -0300

    Décimo Oitavo Commit - teste heroku

--- projetolivraria/src/main/java/br/edu/ifrn/projetolivraria/controller/AutorController.java
+++ projetolivraria/src/main/java/br/edu/ifrn/projetolivraria/controller/AutorController.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.test.web.servlet.result.FlashAttributeResultMatchers;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
