9898c3aa95ca724d457c224089086bdbfc05ccf8
robson-ribeiro-da-silva <robson_r.silva@hotmail.com.br>
2019-05-28 17:09:26 -0300

    Quadragésimo Segundo Commit - teste Heroku mysql 2

--- projetolivraria/src/main/java/br/edu/ifrn/projetolivraria/config/WebSecurityConfig.java
+++ projetolivraria/src/main/java/br/edu/ifrn/projetolivraria/config/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
 				.antMatchers("/http://**").hasAnyAuthority("ADM", "USER")
 				.antMatchers("/https://**").hasAnyAuthority("ADM", "USER")
 				.antMatchers("/usuario/**").hasAnyAuthority("ADM")
-				.antMatchers("${JDBC_DATASOURCE_URL}").permitAll()
+				.antMatchers("/layouts/**").permitAll()
 				.anyRequest().authenticated()
 				.and().formLogin().permitAll()
 				.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
@@ -61,6 +61,6 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/resources/**", "/css/**",  "/vendor/**",  "/img/**",  "/js/**",  "/scss/**", "/h2/**");
-		web.ignoring().antMatchers("${JDBC_DATASOURCE_URL}", "http::/**", "https::/**", "/http::/**", "/https::/**");
+		web.ignoring().antMatchers("/layouts/**", "http::/**", "https::/**", "/http::/**", "/https::/**");
 	}
 }
