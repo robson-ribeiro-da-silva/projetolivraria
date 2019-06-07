279652a01afb51346f8df2829401bb2563da1061
robson-ribeiro-da-silva <robson_r.silva@hotmail.com.br>
2019-06-07 20:24:31 -0300

    commit

--- projetolivraria/src/main/java/br/edu/ifrn/projetolivraria/controller/EmailController.java
+++ projetolivraria/src/main/java/br/edu/ifrn/projetolivraria/controller/EmailController.java
@@ -1,23 +1,18 @@
 
 package br.edu.ifrn.projetolivraria.controller;
 
-import javax.mail.MessagingException;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.edu.ifrn.projetolivraria.service.AutorService;
-import br.edu.ifrn.projetolivraria.service.EmailService;
 
 @Controller
 @RequestMapping("/email")
@@ -33,37 +28,58 @@ public class EmailController {
 	@GetMapping("/enviar")
 	public ModelAndView sendMail() {
 		
-		
-		
-		
-	
-		
 		SimpleMailMessage message = new SimpleMailMessage();
-        message.setText("Testando");
-        message.setSubject("Teste");
+        
+		message.setText("Testando");
+		message.setSubject("Teste");
         message.setTo("robsonrds72@gmail.com");
-        message.setFrom("admlivrariaads@gmail.com");
+        message.setFrom("gestaoescolaronline1.0@gmail.com");
        
-        //mailSender.send(message);
-       
-       
-        System.out.println("controller");
+        try{
+        	System.out.println("entrou");
+        	mailSender.send(message);
+        	System.out.println("foi enviado");
+        }catch (Exception e) {
+        	System.out.println("O email não foi enviado");
+			e.printStackTrace();
+		}
 		
+		/*try{
+			MimeMessage mail = mailSender.createMimeMessage();
+	
+			MimeMessageHelper message = new MimeMessageHelper(mail);
+			
 		
-       
-		/*SimpleMailMessage message = new SimpleMailMessage();
-        message.setText("Testando");
-        message.setTo("admlivrariaads@gmail.com");
-        message.setFrom("admlivrariaads@gmail.com");
-        mailSender.send(message);*/
-      
-        try {
-            mailSender.send(message);
-            System.out.println("Email enviado com sucesso!");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Erro ao enviar email.");
-        }
+			
+			//SimpleMailMessage message = new SimpleMailMessage();
+	        message.setText("Testando");
+	        message.setSubject("Teste");
+	        message.setTo("robsonrds72@gmail.com");
+	       // message.setFrom("admlivrariaads@gmail.com");
+	       
+	        //mailSender.send(message);
+	       
+	       
+	        System.out.println("controller");
+			
+			
+	       
+			SimpleMailMessage message = new SimpleMailMessage();
+	        message.setText("Testando");
+	        message.setTo("admlivrariaads@gmail.com");
+	        message.setFrom("admlivrariaads@gmail.com");
+	        mailSender.send(message);
+	      
+	        
+        mailSender.send(mail);
+        System.out.println("Email enviado com sucesso!");
+	        
+	           
+	           
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("O email não foi enviado");
+		}*/
         
         ModelAndView mv = new ModelAndView("autor/listar");
 		mv.addObject("autors", service.findAll());
